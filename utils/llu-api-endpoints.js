5cd5800b17db9da1ed70eece5a2ecf4a44ceaa3b
Timo Schlüter <timo.schlueter@me.com>
2022-07-14 07:51:11 +0200

    Added the german API endpoint, fixes #54

--- utils/llu-api-endpoints.js
+++ utils/llu-api-endpoints.js
@@ -1,6 +1,7 @@
 const LLU_API_ENDPOINTS = {
     US: "api-us.libreview.io",
     EU: "api-eu.libreview.io",
+    DE: "api-de.libreview.io",
     FR: "api-fr.libreview.io",
     JP: "api-jp.libreview.io",
     AP: "api-ap.libreview.io",
