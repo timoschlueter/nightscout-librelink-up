5f867a9595eb5f84e807c5ba081ebc58280e8d5f
Vladimir Fetisov <vladimir.fetisov@pm.me>
2024-04-04 18:24:22 +0200

    Fix ident

--- src/config.ts
+++ src/config.ts
@@ -14,41 +14,41 @@ function readConfig() {
 	}
 
 	if (process.env.LOG_LEVEL) {
-        if (!['info', 'debug'].includes(process.env.LOG_LEVEL.toLowerCase())) {
+		if (!['info', 'debug'].includes(process.env.LOG_LEVEL.toLowerCase())) {
 			exitLog(`LOG_LEVEL should be either 'info' or 'debug', but got '${process.env.LOG_LEVEL}'`);
-        }
-    }
-    if (process.env.LINK_UP_REGION) {
-        if (!LLU_API_ENDPOINTS.hasOwnProperty(process.env.LINK_UP_REGION)) {
-            exitLog(`LINK_UP_REGION should be one of ${Object.keys(LLU_API_ENDPOINTS)}, but got ${process.env.LINK_UP_REGION}`);
-        }
-    }
-    if (process.env.LINK_UP_TIME_INTERVAL) {
-        if (isNaN(parseInt(process.env.LINK_UP_TIME_INTERVAL))) {
-            exitLog(`LINK_UP_TIME_INTERVAL expected to be an integer, but got '${process.env.LINK_UP_TIME_INTERVAL}'`);
-        }
-    }
+		}
+	}
+	if (process.env.LINK_UP_REGION) {
+		if (!LLU_API_ENDPOINTS.hasOwnProperty(process.env.LINK_UP_REGION)) {
+			exitLog(`LINK_UP_REGION should be one of ${Object.keys(LLU_API_ENDPOINTS)}, but got ${process.env.LINK_UP_REGION}`);
+		}
+	}
+	if (process.env.LINK_UP_TIME_INTERVAL) {
+		if (isNaN(parseInt(process.env.LINK_UP_TIME_INTERVAL))) {
+			exitLog(`LINK_UP_TIME_INTERVAL expected to be an integer, but got '${process.env.LINK_UP_TIME_INTERVAL}'`);
+		}
+	}
 
-    const protocol =
+	const protocol =
 		process.env.NIGHTSCOUT_DISABLE_HTTPS === 'true' ? 'http://' : 'https://';
 	const url = new URL(protocol + process.env.NIGHTSCOUT_URL);
 
 	return {
 		nightscoutApiToken: process.env.NIGHTSCOUT_API_TOKEN as string,
-        nightscoutBaseUrl: url.toString(),
-        linkUpUsername: process.env.LINK_UP_USERNAME as string,
-        linkUpPassword: process.env.LINK_UP_PASSWORD as string,
+		nightscoutBaseUrl: url.toString(),
+		linkUpUsername: process.env.LINK_UP_USERNAME as string,
+		linkUpPassword: process.env.LINK_UP_PASSWORD as string,
 
-        logLevel: process.env.LOG_LEVEL || 'info',
-        singleShot: process.env.SINGLE_SHOT === 'true',
+		logLevel: process.env.LOG_LEVEL || 'info',
+		singleShot: process.env.SINGLE_SHOT === 'true',
 
-        nightscoutApiV3: process.env.NIGHTSCOUT_API_V3 === 'true',
-        nightscoutDisableHttps: process.env.NIGHTSCOUT_DISABLE_HTTPS === 'true',
-        nightscoutDevice: process.env.DEVICE_NAME || 'nightscout-librelink-up',
+		nightscoutApiV3: process.env.NIGHTSCOUT_API_V3 === 'true',
+		nightscoutDisableHttps: process.env.NIGHTSCOUT_DISABLE_HTTPS === 'true',
+		nightscoutDevice: process.env.DEVICE_NAME || 'nightscout-librelink-up',
 
-        linkUpRegion: process.env.LINK_UP_REGION || 'EU',
-        linkUpTimeInterval: Number(process.env.LINK_UP_TIME_INTERVAL) || 5,
-        linkUpConnection: process.env.LINK_UP_CONNECTION as string,
+		linkUpRegion: process.env.LINK_UP_REGION || 'EU',
+		linkUpTimeInterval: Number(process.env.LINK_UP_TIME_INTERVAL) || 5,
+		linkUpConnection: process.env.LINK_UP_CONNECTION as string,
 	};
 }
 
