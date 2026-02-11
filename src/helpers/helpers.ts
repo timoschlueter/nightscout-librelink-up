d1195a1a1d9bc620624abec79d98fbb282054fab
Danny Allen <me@dannya.com>
2026-02-11 23:11:30 +0000

    Fix #221: Implement configurable retry attempts on script error (default: 0 retries, unless configured)

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -6,20 +6,20 @@
 import { Direction } from "../nightscout/interface";
 
 export function mapTrendArrow(libreTrendArrowRaw: number): Direction {
-	switch (libreTrendArrowRaw) {
-		case 1:
-			return Direction.SingleDown;
-		case 2:
-			return Direction.FortyFiveDown;
-		case 3:
-			return Direction.Flat;
-		case 4:
-			return Direction.FortyFiveUp;
-		case 5:
-			return Direction.SingleUp;
-		default:
-			return Direction.NotComputable;
-	}
+    switch (libreTrendArrowRaw) {
+        case 1:
+            return Direction.SingleDown;
+        case 2:
+            return Direction.FortyFiveDown;
+        case 3:
+            return Direction.Flat;
+        case 4:
+            return Direction.FortyFiveUp;
+        case 5:
+            return Direction.SingleUp;
+        default:
+            return Direction.NotComputable;
+    }
 }
 
 export function getUtcDateFromString(timeStamp: string): Date
@@ -27,4 +27,24 @@ export function getUtcDateFromString(timeStamp: string): Date
     const utcDate = new Date(timeStamp);
     utcDate.setTime(utcDate.getTime() - utcDate.getTimezoneOffset() * 60 * 1000);
     return utcDate;
+}
+
+const sleep: (seconds?: number) => Promise<unknown> = (seconds = 0): Promise<unknown> => new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+
+export const retry = async <T>(
+    fn: () => Promise<T> | T,
+    { retryAttempts, retryIntervalSeconds }: { retryAttempts: number; retryIntervalSeconds: number }
+): Promise<T> => {
+    try {
+        return await fn();
+
+    } catch (error) {
+        if (retryAttempts <= 0) {
+            throw error;
+        }
+
+        await sleep(retryIntervalSeconds);
+
+        return retry(fn, { retryAttempts: retryAttempts - 1, retryIntervalSeconds });
+    }
 }
